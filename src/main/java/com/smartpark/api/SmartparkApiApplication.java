2100495d6bf12a24ed2de4de2445a0935157ac53
re-nanashi <jfabro3124@gmail.com>
2026-04-22 23:05:19 +0800

    chore: init springboot application

---
+++ src/main/java/com/smartpark/api/SmartparkApiApplication.java
@@ -0,0 +1,13 @@
+package com.smartpark.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartparkApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartparkApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartpark/api/SmartparkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartpark.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
